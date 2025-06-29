4c1c6ea4cf40a49e36b5b845a650757716e03939
Rahim <rahimch439@gmail.com>
2025-06-30 01:24:20 +0500

    cron job added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import { closeRedisConnection } from "./config/redis.config.js";
 
 const PORT = process.env.PORT || 5000;
 
+const options = {
+  redis: process.env.REDIS_CLOUD_URL,
+};
+const renderQueue = new Queue("render", options);
+
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -34,6 +39,22 @@ const gracefulShutdown = async () => {
     process.exit(1);
   }
 };
+
+app.get("/healthz", async (req, res) => {
+  try {
+    const redisStatus = await renderQueue.checkHealth();
+    if (redisStatus) {
+      return res.sendStatus(200);
+    } else {
+      return res.status(500).json({ message: "Redis connection failed" });
+    }
+  } catch (err) {
+    console.error("Health check failed:", err);
+    return res
+      .status(500)
+      .json({ message: "Redis health check failed", error: err.message });
+  }
+});
 
 // Listen for termination signals
 process.on("SIGINT", gracefulShutdown);
